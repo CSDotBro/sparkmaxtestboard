84c25a03978e9678bcf976d15341ffad7915c0d6
William Jin <wjin0702@yahoo.com>
2020-01-11 15:17:27 -0800

    Add a constructor and run the formatter

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,34 +1,30 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) 2020 FRC Team 3512. All Rights Reserved.
 
-//Implementation code by Adan Silva
+// Implementation code by Adan Silva
 
 #pragma once
 
+#include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
 #include <rev/CANSparkMax.h>
-#include <frc/Joystick.h>
 
 class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void TestPeriodic() override;
+public:
+    Robot();
+    void RobotInit() override;
+    void RobotPeriodic() override;
+    void AutonomousInit() override;
+    void AutonomousPeriodic() override;
+    void TeleopInit() override;
+    void TeleopPeriodic() override;
+    void TestPeriodic() override;
 
- private:
+private:
     frc::Joystick joystick0{0};
-    //frc::Joystick joystick1{1};
-    rev::CANEncoder  encoder0;
-    rev::CANEncoder  encoder1;
+    // frc::Joystick joystick1{1};
+    rev::CANEncoder encoder0;
+    rev::CANEncoder encoder1;
     rev::CANPIDController controller0;
     rev::CANPIDController controller1;
     rev::CANSparkMax spark0{14, rev::CANSparkMax::MotorType::kBrushless};
